8b633094bb9bfaacd334d6e9b4bd26e23e857b8b
dmc21 <davidmoracaceres20@gmail.com>
2022-02-24 20:07:13 +0100

    improve web dev score

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import '../styles/globals.css'
-
+import Head from 'next/head';
 
 import { NextUIProvider } from '@nextui-org/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <NextUIProvider defaultTheme="dark" justify="center" alignItems="center">
+      <Head>
+        <title>NextJS App</title>
+        <meta charset="utf-8"/>
+        <meta name="description" content="Serverless NextJS App using firebase as server"/>
+        <meta name="author" content="David Mora Cáceres" />
+      </Head>
       <Component {...pageProps} />
     </NextUIProvider>
   )
